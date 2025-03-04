91e554c6e994d92f3ebe4ad1d0375163fbb1375c
Shantanu_Nagwekar <shantanunagwekar12@gmail.com>
2025-03-04 21:09:48 +0530

    Solved TwoSum using hashmaps in Python

--- TwoSum.py
+++ TwoSum.py
@@ -1,11 +1,13 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        hashmap = {}
+        hashmap = {} #initializing empty hashmap
+        #populating hashmap
         for index, num in enumerate(nums):
             hashmap[num] = index
         for index, num in enumerate(nums):
-            n = target - num
-            if n in hashmap and hashmap[n] != index:
+            n = target - num # finding complement 
+            if n in hashmap and hashmap[n] != index: 
+                #checking whether complement present in hashmap and ensuring we don't use the same element twice 
                 return [index, hashmap[n]]
 
 sol = Solution()
