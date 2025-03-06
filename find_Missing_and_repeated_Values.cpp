689357ab391cefadacbc1bf60b3be5adfd5a4327
Shantanu_Nagwekar <shantanunagwekar12@gmail.com>
2025-03-06 21:43:52 +0530

    Solved find missing and repeated values problem

---
+++ find_Missing_and_repeated_Values.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        vector<int> hash(n * n, 0);
+        int repeat, miss;
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                hash[grid[row][col] - 1]++;
+            }
+        }
+        for (int i = 0; i < n * n; i++)
+        {
+            if (hash[i] == 0)
+            {
+                miss = i + 1;
+            }
+            if (hash[i] == 2)
+            {
+                repeat = i + 1;
+            }
+        }
+        return {repeat, miss};
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> Grid = {{1, 3}, {2, 2}};
+    vector<int> result = sol.findMissingAndRepeatedValues(Grid);
+    cout<<"[";
+    for (int x : result) {
+        cout<<x<<" ";
+    }
+    cout<<"]"<<endl;
+}
